278661fe90ba71eefdefd411044a9df32f20ed7b
DanilloRezende <danillo.rezende3@gmail.com>
2022-08-30 10:26:51 -0300

    Funcionalidades da aplicação - Iniciando novo ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from "zod"
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separetor, StartCountdownButton, Taskinput } from "./styles";
+import { useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -23,7 +24,16 @@ const newCycleFormValidationSchema = zod.object({
 // type é melhor utilizado para quando inferimos a tipagem de um objeto que já existe 
 type NewCycleFormAmount = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCyclesId, setActiveCyclesId] = useState<string | null>(null);
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormAmount>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -33,10 +43,26 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCycleFormAmount) {
-    console.log(data)
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    // Poderiamos utilizar o spreed operator para incluir no novo valor os valores antigos pois se trata de uma nova lista
+    // Porem como semrpe que utilizamos uma função quando atualizamos um estado e ele depende da informação anterior
+    // Devemos setar atraves de uma arrow function conceito de clousures.
+    setCycles((state) => [...state, newCycle])
+    setActiveCyclesId(id)
+
     reset();
   }
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCyclesId)
+
+  console.log(activeCycle)
 
   const task = watch("task")
   const isSubmitDisabled = !task;
